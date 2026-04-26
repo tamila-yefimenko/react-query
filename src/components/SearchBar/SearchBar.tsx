2fa80ea73f0a7a5b6b0cc600ab6613897be683c5
Tamila Yefimenko <tamilaedu88@gmail.com>
2026-04-26 20:56:46 +0300

    refactor SearchBar with formik and yup

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,36 @@
+import { Formik, Form, Field, type FormikHelpers, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import css from './SearchBar.module.css';
 
 interface SearchBarProps {
   onSubmit: (query: string) => void;
 }
 
+interface FormValues {
+  query: string;
+}
+
 const SearchBar = ({ onSubmit }: SearchBarProps) => {
-  const handleSubmit = (formData: FormData) => {
-    const value = formData.get('query') as string;
+  const initialValues: FormValues = {
+    query: '',
+  };
+
+  const Schema = Yup.object().shape({
+    query: Yup.string()
+      .min(2, 'Search query must be at least 2 symbols')
+      .max(15, 'Too long seqrch query')
+      .required('Query is required'),
+  });
+
+  const handleSubmit = (
+    values: FormValues,
+    actions: FormikHelpers<FormValues>
+  ) => {
+    const value = values.query;
 
     onSubmit(value);
+
+    actions.resetForm();
   };
 
   return (
@@ -22,19 +44,34 @@ const SearchBar = ({ onSubmit }: SearchBarProps) => {
         >
           Powered by TMDB
         </a>
-        <form className={css.form} action={handleSubmit}>
-          <input
-            className={css.input}
-            type="text"
-            name="query"
-            autoComplete="off"
-            placeholder="Search movies..."
-            autoFocus
-          />
-          <button className={css.button} type="submit">
-            Search
-          </button>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={Schema}
+          onSubmit={handleSubmit}
+        >
+          <Form className={css.form}>
+            <div className={css.wrapper}>
+              <Field
+                className={css.input}
+                type="text"
+                name="query"
+                autoComplete="off"
+                placeholder="Search movies..."
+                autoFocus
+              />
+              <button className={css.button} type="submit">
+                Search
+              </button>
+              <div className={css.errorWrapper}>
+                <ErrorMessage
+                  name="query"
+                  component="span"
+                  className={css.error}
+                />
+              </div>
+            </div>
+          </Form>
+        </Formik>
       </div>
     </header>
   );
